f5da544139b496fa3c664c25a76e897aeb2a5e0a
thankkingdom <thankkingdom@gmail.com>
2020-11-17 17:01:50 +0900

    Initial commit

---
+++ src/main/java/com/example/web/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.example.web.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+
+    public String index() {
+
+        return "index";
+    }
+}
