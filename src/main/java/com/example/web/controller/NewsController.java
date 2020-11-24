552d85219c345c8ab8ca58f5e08599904ef1047e
thankkingdom <thankkingdom@gmail.com>
2020-11-24 15:42:47 +0900

    Thymeleaf テンプレート化

---
+++ src/main/java/com/example/web/controller/NewsController.java
@@ -0,0 +1,16 @@
+package com.example.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("news")
+public class NewsController {
+
+    @GetMapping
+    public String index() {
+
+        return "news/index";
+    }
+}
